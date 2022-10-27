319db9ddd49afc42bc0cfaf3001b4e71467159c5
JaquelineSC <jscnewlf@gmail.com>
2022-10-27 20:30:53 -0300

    Subindo projeto base

---
+++ Models/Lanche.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    [Table("Lanches")]
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+
+        [Required]
+        [Display(Name = "Nome do Lanche")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage="sdfsdfsdfsdf")]
+        [Display(Name = "Nome do Lanche")]
+        [MinLength(20, ErrorMessage = "sdfsdfsdfsdf")]
+        [MaxLength(200)]
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }  
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }   
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; } 
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }    
+    }
+}
